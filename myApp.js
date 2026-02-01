9383711a405ee71d660777f45de9c37fa1684384
DKJazzu <dkjazzu@gmail.com>
2026-02-01 19:26:26 +0200

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,50 +1,12 @@
 let express = require('express');
 let app = express();
-require('dotenv').config();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-app.get("/json", function (req, res) {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-  res.json({ message: message });
-});
-
-app.get("/now", function (req, res, next) {
-  req.time = new Date().toString();
-  next();
-}, function (req, res) {
-  res.json({ time: req.time });
-});
-
-app.get("/:word/echo", function (req, res) {
-  res.json({ echo: req.params.word });
-});
-
-app.get("/name", function (req, res) {
-  const first = req.query.first;
-  const last = req.query.last;
-  res.json({ name: first + " " + last });
-});
-
-app.post("/name", function (req, res) {
-  const first = req.body.first;
-  const last = req.body.last;
-  res.json({ name: first + " " + last });
+app.post("/name", function(req, res) {
+  let string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
 });
 
 module.exports = app;
